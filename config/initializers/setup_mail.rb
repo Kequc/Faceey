a57063082c304f1e5a060a892704d8530179ea19
Nathan Lunde <nathanlunde@nathan-lundes-macbook-pro.local>
2011-02-11 01:53:19 +0000

    Got email working

--- config/initializers/setup_mail.rb
+++
@@ -1,7 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-  :address => "localhost",
-  :port => 1025,
-  :domain => APP[:site_domain]
-}
-
-ActionMailer::Base.default_url_options[:host] = "localhost:3000"
