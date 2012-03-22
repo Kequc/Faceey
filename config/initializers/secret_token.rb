3b0be714247a548f98bbf54b29b94602d2f7e172
Nathan Lunde-Berry <nathan.lundeberry@gmail.com>
2012-03-22 20:16:18 +0400

    Cheged secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,6 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Faceey::Application.config.secret_token = 'a36e1a302c89634f75d5f2f803c9ad163615a07773436f0421036bddd316cb0ffa6016424abd916edc27198e87c6d428268319e5b1f9a5d48e70b6f53c29720e'
+
+# This is different than the one running at the live site
+Faceey::Application.config.secret_token = 'a36e1a302c89634f75d5f2f103c9ad163615a07773436f0421036bddd316cb0ffa6016424abd916edc27198e87c6d428268319e5b1f9a5d48e70b6f53c29720e'
